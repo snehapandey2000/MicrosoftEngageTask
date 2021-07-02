ec08aab3bf6b87a669f6ca74af8aba7235400be1
Sneha Pandey <snehapandey2000@users.noreply.github.com>
2021-07-03 02:35:52 +0530

    Changes done

--- src/Video.js
+++ src/Video.js
@@ -20,7 +20,7 @@ import Modal from 'react-bootstrap/Modal'
 import 'bootstrap/dist/css/bootstrap.css'
 import "./Video.css"
 
-const server_url = process.env.NODE_ENV === 'production' ? 'https://microsoft-clone-sneha.netlify.app/' : "http://localhost:4001"
+const server_url = process.env.NODE_ENV === 'production' ? 'https://my-microsoft-teams-clone.herokuapp.com/' : "http://localhost:4001"
 
 var connections = {}
 const peerConnectionConfig = {
